668a816b46818da431f215385df9f7d4b6546b84
Monique Brown <goodolmo@icloud.com>
2022-05-17 08:46:14 -0400

    correcting templates and adding in layouts

--- webapp.py
+++ webapp.py
@@ -1,11 +1,19 @@
+from xml.sax.handler import property_declaration_handler
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from forms import AddForm, DelForm
+from form import AddForm, DelForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import pymysql
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my secret key"
+
+engine ='mysql+pymysql://root:pw123@localhost/tractortek'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = engine
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -16,16 +24,22 @@ class Employees(db.Model):
 
     __tablename__ = "employees"
 
-    emp_id = db.Column(db.StringField, primary_key=True)
-    team_lead = db.Column(db.StringField)
-    pay_grd = db.Column(db.StringField)
-    region = db.Column(db.StringField)
+    emp_id = db.Column(db.Text, primary_key=True)
+    name = db.Column(db.Text)
+    pay_grd = db.Column(db.Text)
+    region = db.Column(db.Text)
 
-    def __init__(self, emp_id, team_lead, pay_grd, region):
+    def __init__(self, emp_id, name, pay_grd, region):
         self.emp_id = emp_id
-        self.team_lead = team_lead
+        self.name = name
         self.pay_grd = pay_grd
         self.region = region
+
+
+
+#class Sales(db.Model):
+
+   # __tablename__ = 'sales'
 
 
 
@@ -40,7 +54,7 @@ class Employees(db.Model):
 #home page
 @app.route('/')
 def index():
-    return "<h1>text here</h1>"
+    return render_template('index.html')
 
 
 
@@ -54,14 +68,32 @@ def about():
 #page to add sales data
 @app.route('/add_sales')
 def add_sales():
-    return " "
+    form = AddForm()
+
+    #if form.validate_on_submit()
+
+
+    return render_template('add_sales.html', form=form)
+
+
+
+#page to delete sales data
+@app.route('/delete')
+def del_sales():
+    form = DelForm()
+    return render_template('del_sales.html', form=form)
+
+
 
 
 
 #page to view yearly sales data
-@app.route('/sales/<year>')
-def sales(year):
-    return "<h2> sales data for {} is listed here </h2>".format(year)
+@app.route('/list')
+def sales_list():
+
+    return render_template('sales_list.html')
+
+
 
 
 
